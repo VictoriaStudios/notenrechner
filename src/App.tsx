2ebfddf1c203f912e1f6a7cf0927460d50eab3be
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-06-18 21:01:49 -0500

    Finished basic functionality and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
+import { percentages } from './utils/percentages';
 
 function App() {
+  const [maxPoints, setMaxPoints] = useState<string | number | readonly string[] | undefined>(24)
+  const [currentPoints, setCurrentPoints] = useState<string | number | readonly string[] | undefined>(24)
+  const getGrade = () => {
+    let fraction =  Number(currentPoints)/ Number(maxPoints)
+    if (fraction>=percentages.sieben) return 7
+    if (fraction>=percentages.sechs) return 6
+    if (fraction>=percentages.vier) return 4
+    if (fraction>=percentages.fuenf) return 5
+    if (fraction>=percentages.drei) return 3
+    if (fraction>=percentages.zwei) return 2
+    if (fraction>=percentages.eins) return 1
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notenrechner basierend auf IB Punkten</h1>
+      <h3>Maximale Punkte:</h3>
+      <input type='number' value={maxPoints} onChange={(e) => setMaxPoints(e.target.value)} />
+      <h3>Geschaffte Punkte:</h3>
+      <input type='number' value={currentPoints} onChange={(e) => setCurrentPoints(e.target.value)} />
+      <h3>Note: {getGrade()}</h3>
     </div>
   );
 }
---
+++ src/utils/percentages.ts
@@ -0,0 +1,9 @@
+export const percentages = {
+    sieben: .75,
+    sechs: .625,
+    fuenf: .5,
+    vier: .375,
+    drei: .25,
+    zwei: .1,
+    eins: 0
+}
