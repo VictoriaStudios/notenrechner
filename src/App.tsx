feffb69015458e3c43ad4186a88853605084d93c
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-06-19 19:50:04 -0500

    Finished app

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,53 @@ import './App.css';
 import { useState } from 'react';
 import React from 'react';
 import { percentages } from './utils/percentages';
+import { percentagesPeru } from './utils/percentages';
 
 function App() {
   const [maxPoints, setMaxPoints] = useState<string | number | readonly string[] | undefined>(24)
   const [currentPoints, setCurrentPoints] = useState<string | number | readonly string[] | undefined>(24)
   const getGrade = () => {
-    let fraction =  Number(currentPoints)/ Number(maxPoints)
-    if (fraction>=percentages.sieben) return 7
-    if (fraction>=percentages.sechs) return 6
-    if (fraction>=percentages.vier) return 4
-    if (fraction>=percentages.fuenf) return 5
-    if (fraction>=percentages.drei) return 3
-    if (fraction>=percentages.zwei) return 2
-    if (fraction>=percentages.eins) return 1
+    let fraction = Number(currentPoints) / Number(maxPoints)
+    if (fraction >= percentages.sieben) return 7
+    if (fraction >= percentages.sechs) return 6
+    if (fraction >= percentages.vier) return 4
+    if (fraction >= percentages.fuenf) return 5
+    if (fraction >= percentages.drei) return 3
+    if (fraction >= percentages.zwei) return 2
+    if (fraction >= percentages.eins) return 1
+  }
+
+  const getGradePeru = () => {
+    let fraction = Number(currentPoints) / Number(maxPoints)
+    if (fraction >= percentagesPeru.zwanzig) return 20
+    if (fraction >= percentagesPeru.neunzehn) return 19
+    if (fraction >= percentagesPeru.achtzehn) return 18
+    if (fraction >= percentagesPeru.siebzehn) return 17
+    if (fraction >= percentagesPeru.sechszehn) return 16
+    if (fraction >= percentagesPeru.fuenfzehn) return 15
+    if (fraction >= percentagesPeru.vierzehn) return 14
+    if (fraction >= percentagesPeru.dreizehn) return 13
+    if (fraction >= percentagesPeru.zwoelf) return 12
+    if (fraction >= percentagesPeru.elf) return 11
+    if (fraction >= percentagesPeru.zehn) return 10
+    if (fraction >= percentagesPeru.neun) return 9
+    if (fraction >= percentagesPeru.acht) return 8
+    if (fraction >= percentagesPeru.sieben) return 7
+    if (fraction >= percentagesPeru.sechs) return 6
+    if (fraction >= percentagesPeru.fuenf) return 5
   }
 
   return (
     <div className="App">
-      <h1>Notenrechner basierend auf IB Punkten</h1>
+      <h1>Notenrechner basierend auf IB Punkten - Klausur 1</h1>
       <h3>Maximale Punkte:</h3>
       <input type='number' value={maxPoints} onChange={(e) => setMaxPoints(e.target.value)} />
       <h3>Geschaffte Punkte:</h3>
       <input type='number' value={currentPoints} onChange={(e) => setCurrentPoints(e.target.value)} />
-      <h3>Note: {getGrade()}</h3>
+      <div className="gradeContainer">
+        <h3 className='gradeDisplay'>Note Peru: {getGradePeru()}</h3>
+        <h3 className='gradeDisplay'>Note IB: {getGrade()}</h3>
+      </div>
     </div>
   );
 }
--- src/utils/percentages.ts
+++ src/utils/percentages.ts
@@ -7,3 +7,22 @@ export const percentages = {
     zwei: .1,
     eins: 0
 }
+
+export const percentagesPeru = {
+    zwanzig: .82,
+    neunzehn: .75,
+    achtzehn: .6875,
+    siebzehn: .625,
+    sechszehn: .5625,
+    fuenfzehn: .5,
+    vierzehn: .4375,
+    dreizehn: .375,
+    zwoelf: .3125,
+    elf: .25,
+    zehn: .2,
+    neun: .15,
+    acht: .1,
+    sieben: .066,
+    sechs: .033,
+    fuenf: 0,
+}
